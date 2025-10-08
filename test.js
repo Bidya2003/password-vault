bd6cf64a697909323d7ac1219799c4651745c3b1
Bidya2003 <bidyajanimaji2003@gmail.com>
2025-10-08 14:10:45 +0530

    remove test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const uri = "mongodb+srv://passwordVault:PassVau0710@cluster0.e85xh.mongodb.net/passwordVaultDB?retryWrites=true&w=majority";
-
-mongoose.connect(uri)
-  .then(() => console.log("MongoDB connected!"))
-  .catch((err) => console.error("MongoDB connection error:", err));
